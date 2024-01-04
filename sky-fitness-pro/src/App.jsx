c2f0ec006d278aa59b0b65cd7e76f665c72ef76a
karnaukhoff <n4echduf@gmail.com>
2024-01-04 18:24:59 +0300

    Профиль

--- sky-fitness-pro/src/App.jsx
+++ sky-fitness-pro/src/App.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import './App.css';
+import { createGlobalStyle } from "styled-components";
 import { AppRoutes } from "./routes";
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    @font-face {
+      font-family: 'StratosSkyeng';
+      src: local("StratosSkyeng"), local("StratosSkyeng"), url("/fonts/StratosSkyeng.woff2") format("woff2"), url("/fonts/StratosSkyeng.woff") format("woff");
+      font-weight: 400;
+      font-style: normal;
+    }
+  }
+`;
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <AppRoutes />
     </>
   );
---
+++ sky-fitness-pro/src/pages/styles/ProfileStyle.js
@@ -0,0 +1,6 @@
+import { styled } from "styled-components";
+
+export const ProfileContainer = styled.div`
+    margin: 75px;
+    margin-top: 35px;
+`
