13ab1bce1ad8875d24dea7e80b28d4bd9cb222d7
karnaukhoff <n4echduf@gmail.com>
2024-01-13 00:37:19 +0300

    Глвная страница + нужны доработки

--- sky-fitness-pro/src/routes.jsx
+++ sky-fitness-pro/src/routes.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Authorization } from "./pages/Authorization";
 import { Profile } from "./pages/Profile";
+//import { WorkoutPage } from "./pages/Workout";
+import MainPage from "./pages/MainPage";
 
 export const AppRoutes = () => {
 
@@ -9,6 +11,7 @@ export const AppRoutes = () => {
         <Routes>
             <Route path="/login" element={<Authorization />} />
             <Route path="/profile" element={<Profile />} />
+            <Route  path="/" element={<MainPage />} />
         </Routes>
     )
 }
